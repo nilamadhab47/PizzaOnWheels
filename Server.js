015f328474099a23d6889433e1e44b0d09bd4f48
Nilamadhab <nilamadhab47@gmail.com>
2022-08-09 03:42:29 +0530

    commit

--- Server.js
+++ Server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV ==='production')
 {
     app.use('/' , express.static('client_side/build'))
 
-    app.get('*' , (req , res)=>{
+    app.get('/*' , (req , res)=>{
 
         res.sendFile(path.resolve(__dirname, 'client_side', "build", 'index.html'))
 
